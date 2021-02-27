7e7a0eda69f0c11ad7e74856226849b28e361aa0
Chris Mason <m1achris43@gmail.com>
2021-02-26 23:48:18 -0500

    init commit

---
+++ HelloWorld/src/HelloWorldApp.java
@@ -0,0 +1,12 @@
+
+public class HelloWorldApp {
+
+	public static void main(String[] args) {
+		//method is a collection of statements that performs an operation
+		//void means the method will not return anything
+		// System.out.println("Hello World!");
+		System.out.println("Hello, Chris!");
+
+	}
+
+}
